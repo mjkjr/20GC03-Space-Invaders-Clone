11972fc955c79e1c49284f4def237814151e5590
Michael J. Kozubal Jr <mjkjr84@gmail.com>
2024-11-13 17:51:46 -0500

    Adjusted enemy layout and added initial mothership WIP

--- enemies/mothership.gd
+++ enemies/mothership.gd
@@ -11,39 +11,33 @@ var movement_direction: Direction = Direction.RIGHT
 
 func _ready() -> void:
 	# randomize the shooting timer
-	$Shoot.wait_time = randf_range(1, 120)
+	$Shoot.wait_time = randf_range(1, 5)
 	$Shoot.start()
 
 
-func set_type(which: int) -> void:
-	$Sprite2D.frame = which
+func _process(_delta: float) -> void:
+	pass
 
 
-func move() -> void:
+func _on_move_timeout() -> void:
 	if movement_direction == Direction.LEFT:
-		if position.x == $Sprite2D.get_rect().size.x:
-			position.y += $Sprite2D.get_rect().size.y
+		if position.x == 0:
 			movement_direction = Direction.RIGHT
 		else:
 			position.x -= $Sprite2D.get_rect().size.x
 	else:
-		if position.x == get_viewport_rect().size.x - ($Sprite2D.get_rect().size.x * 2):
-			position.y += $Sprite2D.get_rect().size.y
+		if position.x == get_viewport_rect().size.x - $Sprite2D.get_rect().size.x:
 			movement_direction = Direction.LEFT
 		else:
 			position.x += $Sprite2D.get_rect().size.x
 
 
-func _on_move_timeout() -> void:
-	move()
-
-
 func _on_shoot_timeout() -> void:
 	var projectile = PROJECTILE.instantiate()
-	projectile.position.x = global_position.x + 33
-	projectile.position.y = global_position.y + 21
+	projectile.position.x = global_position.x + 48
+	projectile.position.y = global_position.y + 6
 	add_sibling(projectile)
-	$Shoot.wait_time = randf_range(1, 120)
+	$Shoot.wait_time = randf_range(1, 5)
 	$Shoot.start()
 
 
--- main.gd
+++ main.gd
@@ -1,30 +1,38 @@
 extends Node
 
 const ENEMY = preload("res://enemies/enemy.tscn")
-const INITAL_ENEMY_COUNT = 50
+const MOTHERSHIP = preload("res://enemies/mothership.tscn")
 
 var score: int = 0
 
 
 func _ready() -> void:
-	for i in range(0, INITAL_ENEMY_COUNT):
-		get_tree().call_group("enemies", "move")
-		var new_enemy = ENEMY.instantiate()
-		add_child(new_enemy)
-		new_enemy.killed.connect(_on_enemy_killed)
+	for i in range(0, 3):
+		for n in range(0, 21):
+			get_tree().call_group("enemies", "move")
+			var new_enemy = ENEMY.instantiate()
+			new_enemy.set_type(i)
+			add_child(new_enemy)
+			new_enemy.position.x = new_enemy.get_node("Sprite2D").get_rect().size.x
+			new_enemy.position.y = 2 * new_enemy.get_node("Sprite2D").get_rect().size.y
+			new_enemy.destroyed.connect(_on_enemy_destroyed)
+	$Mothership.wait_time = randf_range(0, 1)
+	$Mothership.start()
 
 
-func _process(_delta: float) -> void:
-	pass
-
-
-func _on_enemy_timer_timeout() -> void:
-	get_tree().call_group("enemies", "move")
-	var new_enemy = ENEMY.instantiate()
-	add_child(new_enemy)
-	new_enemy.killed.connect(_on_enemy_killed)
-
-
-func _on_enemy_killed() -> void:
+func _on_enemy_destroyed() -> void:
 	score += 100
 	print("Score: %s" % score)
+
+
+func _on_mothership_timeout() -> void:
+	var mothership = MOTHERSHIP.instantiate()
+	add_child(mothership)
+	mothership.position.x = -mothership.get_node("Sprite2D").get_rect().size.x
+	mothership.position.y = 0.5 * mothership.get_node("Sprite2D").get_rect().size.y
+	mothership.destroyed.connect(_on_mothership_destroyed)
+
+
+func _on_mothership_destroyed() -> void:
+	$Mothership.wait_time = randf_range(30, 90)
+	$Mothership.start()
